6a4682ceb004963b32cb2c0e1a5c9fc0dd22807e
Dmitri Grosu <dmitri.grosu@codefactorygroup.com>
2021-02-10 21:34:13 +0200

    goods and order proto; additional columns and indexes in 'good' table

--- src/main/resources/create_db.sql
+++ src/main/resources/create_db.sql
@@ -1,48 +1,71 @@
 create table if not exists rama_fm.brand
 (
-    id         serial      not null
+    id         serial                   not null
         constraint brand_pk
             primary key,
-    name       text        not null,
-    created_at timestamp with time zone not null default now(),
-    deleted_at timestamp with time zone
-);
-
-create table if not exists rama_fm.good
-(
-    id         serial      not null
-        constraint good_pk
-            primary key,
-    name       text        not null,
-    price      numeric              default 0 not null,
-    brand_id   integer
-        constraint good_brand_id_fk
-            references rama_fm.brand(id),
-    created_at timestamp with time zone not null default now(),
-    deleted_at timestamp with time zone
-);
-
-create table if not exists rama_fm.client
-(
-    id         serial      not null
-        constraint client_pk
-            primary key,
-    name       text        not null,
-    idno       char(13)    not null,
+    name       text                     not null,
+    erp_code   text,
     created_at timestamp with time zone not null default now(),
     deleted_at timestamp with time zone
 );
 
 create table if not exists rama_fm.good_group
 (
-    id              serial      not null
+    id              serial                   not null
         constraint good_group_pk
             primary key,
-    name            text        not null,
+    name            text                     not null,
     parent_group_id integer,
+    erp_code        text,
     created_at      timestamp with time zone not null default now(),
     deleted_at      timestamp with time zone
 );
+
+create unique index good_group_erp_code_uindex
+    on rama_fm.good_group (erp_code);
+
+create table if not exists rama_fm.good
+(
+    id         serial                   not null
+        constraint good_pk
+            primary key,
+    name       text                     not null,
+    price      numeric                           default 0 not null,
+    brand_id   integer
+        constraint good_brand_id_fk
+            references rama_fm.brand (id),
+    group_id   integer
+        constraint good_group_id_fk
+            references rama_fm.good_group (id),
+    unit       text,
+    package    numeric,
+    erp_code   text,
+    bar_code   text,
+    weight     numeric,
+    created_at timestamp with time zone not null default now(),
+    deleted_at timestamp with time zone
+);
+
+create unique index good_erp_code_uindex
+    on rama_fm.good (erp_code);
+create index good_brand_id_index
+    on rama_fm.good (brand_id);
+create index good_group_id_index
+    on rama_fm.good (group_id);
+
+create table if not exists rama_fm.client
+(
+    id         serial                   not null
+        constraint client_pk
+            primary key,
+    name       text                     not null,
+    idno       char(13)                 not null,
+    created_at timestamp with time zone not null default now(),
+    deleted_at timestamp with time zone
+);
+
+create unique index client_idno_uindex
+    on rama_fm.client (idno);
 
 create table if not exists rama_fm."order"
 (
@@ -51,52 +74,52 @@ create table if not exists rama_fm."order"
             primary key,
     client_id  integer
         constraint order_client_id_fk
-            references rama_fm.client(id),
-    total_sum  numeric default 0        not null,
+            references rama_fm.client (id),
+    total_sum  numeric                           default 0 not null,
     created_at timestamp with time zone not null default now(),
     deleted_at timestamp with time zone
 );
 
 create table if not exists rama_fm.order_good
 (
-    id       serial            not null
+    id         serial                   not null
         constraint order_good_pk
             primary key,
-    order_id integer           not null
+    order_id   integer                  not null
         constraint order_good_order_id_fk
-            references rama_fm.order(id),
-    good_id  integer           not null
+            references rama_fm.order (id),
+    good_id    integer                  not null
         constraint order_good_good_id_fk
-            references rama_fm.good(id),
-    quantity numeric default 0 not null,
-    sum      numeric default 0 not null,
+            references rama_fm.good (id),
+    quantity   numeric default 0        not null,
+    sum        numeric default 0        not null,
     created_at timestamp with time zone not null default now(),
     deleted_at timestamp with time zone
 );
 
-create table if not exists "app_user"
+create table if not exists rama_fm."app_user"
 (
-    id serial                             not null
+    id         serial                   not null
         constraint app_user_pk
             primary key,
-    email      text                    not null,
-    passwd     text                    not null,
+    email      text                     not null,
+    passwd     text                     not null,
     client_id  integer
         constraint app_user_client_id_fk
-            references rama_fm.client(id),
+            references rama_fm.client (id),
     created_at timestamp with time zone not null default now(),
     deleted_at timestamp with time zone
 );
 
-create table if not exists "app_user_role"
+create table if not exists rama_fm."app_user_role"
 (
-    id         serial                     not null
+    id         serial                   not null
         constraint app_user_role_pk
             primary key,
-    "user_id"  integer                 not null
+    "user_id"  integer                  not null
         constraint app_role_app_user_id_fk
-            references rama_fm.app_user(id),
-    role       text                    not null,
+            references rama_fm.app_user (id),
+    role       text                     not null,
     created_at timestamp with time zone not null default now(),
     deleted_at timestamp with time zone
 );
--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -4,8 +4,21 @@ create table if not exists "brand"
         constraint brand_pk
             primary key auto_increment,
     name       text      not null,
+    erp_code   text,
     created_at timestamp not null default now(),
-    deleted_at timestamp 
+    deleted_at timestamp
+);
+
+create table if not exists "good_group"
+(
+    id              int       not null
+        constraint good_group_pk
+            primary key auto_increment,
+    name            text      not null,
+    parent_group_id integer,
+    erp_code        text,
+    created_at      timestamp not null default now(),
+    deleted_at      timestamp
 );
 
 create table if not exists "good"
@@ -18,30 +31,27 @@ create table if not exists "good"
     brand_id   integer
         constraint good_brand_id_fk
             references "brand",
+    group_id   integer
+        constraint good_group_id_fk
+            references "good_group" (id),
+    unit       text,
+    package    numeric,
+    erp_code   text,
+    bar_code   text,
+    weight     numeric,
     created_at timestamp not null default now(),
-    deleted_at timestamp 
+    deleted_at timestamp
 );
 
 create table if not exists "client"
 (
-    id         int       not null
+    id           int       not null
         constraint client_pk
             primary key auto_increment,
-    name       text      not null,
-    idno       char(13)  not null,
+    name         text      not null,
+    idno         char(13)  not null,
     "created_at" timestamp not null default now(),
     "deleted_at" timestamp
-);
-
-create table if not exists "good_group"
-(
-    id              int       not null
-        constraint good_group_pk
-            primary key auto_increment,
-    name            text      not null,
-    parent_group_id integer,
-    created_at      timestamp not null default now(),
-    deleted_at      timestamp 
 );
 
 create table if not exists "order"
@@ -54,7 +64,7 @@ create table if not exists "order"
             references "client",
     total_sum  numeric            default 0 not null,
     created_at timestamp not null default now(),
-    deleted_at timestamp 
+    deleted_at timestamp
 );
 
 create table if not exists "order_good"
@@ -72,11 +82,11 @@ create table if not exists "order_good"
 
 create table if not exists "app_user"
 (
-    id         int       not null
+    id           int       not null
         constraint app_user_pk
             primary key auto_increment,
-    email      text      not null,
-    passwd     text      not null,
+    email        text      not null,
+    passwd       text      not null,
     "client_id"  integer
         constraint app_user_client_id_fk
             references "client",
@@ -92,5 +102,5 @@ create table if not exists "app_user_role"
     "user_id"  integer   not null,
     role       text      not null,
     created_at timestamp not null default now(),
-    deleted_at timestamp 
+    deleted_at timestamp
 );
