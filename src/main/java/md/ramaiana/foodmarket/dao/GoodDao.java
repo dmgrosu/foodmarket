7794ac7321187f04f7428e3709193b98a12308d2
Dmitri Grosu <dmitri.grosu@codefactorygroup.com>
2021-02-12 10:22:19 +0200

    updated good proto; changed goods dao to paginated

--- src/main/java/md/ramaiana/foodmarket/dao/GoodDao.java
+++ src/main/java/md/ramaiana/foodmarket/dao/GoodDao.java
@@ -1,12 +1,12 @@
 package md.ramaiana.foodmarket.dao;
 
 import md.ramaiana.foodmarket.model.Good;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author Dmitri Grosu (dmitri.grosu@gmail.com), 2/10/21
  */
 @Repository
-public interface GoodDao extends CrudRepository<Good, Integer> {
+public interface GoodDao extends PagingAndSortingRepository<Good, Integer> {
 }
